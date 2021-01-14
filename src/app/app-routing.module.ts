3d952b35dc9c1fc4eeac7493dd5b3aa151627268
Nmuta Jones <nmuta1@gmail.com>
2021-01-14 16:56:04 +0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlightsComponent } from './flights/flights.component';
 
-const routes: Routes = [];
+const routes: Routes = [{component: FlightsComponent, path : "flights" }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
