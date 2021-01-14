c5b1e595522f239b991bc43503ffbd81ec8bad0a
Nmuta Jones <nmuta1@gmail.com>
2021-01-14 17:02:44 +0300

    Remove ref from module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlightsComponent } from './flights/flights.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlightsComponent
   ],
   imports: [
     BrowserModule,
