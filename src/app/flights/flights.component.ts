b004eb2f41b19dd057730a2d1128db95e6175cc7
Nmuta Jones <nmuta1@gmail.com>
2021-01-14 17:47:06 +0300

    Move flights to its own component

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Flight } from './flight.model';
 
 @Component({
   selector: 'app-flights',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flights.component.scss']
 })
 export class FlightsComponent implements OnInit {
+
+  flights: Flight[] = [
+    {origin: "Denver", destination: "Miami", flightNumber: 5, 
+    depart: new Date(), arrive: new Date(), nonstop: true},
+    {origin: "Phoenix", destination: "NYC", flightNumber: 5, 
+    depart: new Date(), arrive: new Date(), nonstop: true, snacks: "crackers"},
+  ];
 
   constructor() { }
 
